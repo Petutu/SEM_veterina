e915c1f816c6ca5e52f07187751eef86ff3f73ed
Petutu <st69646@upce.cz>
2024-11-04 17:49:26 +0100

    Revert "ignore this"
    This reverts commit c76830157d0c04c6dabd0a1ac06e20b6d9c83742.

--- Sem_Veterina/Program.cs
+++ Sem_Veterina/Program.cs
@@ -1,21 +1,122 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Sem_Veterina;
+using Sem_Veterina.Controllers;
+using Oracle.ManagedDataAccess.Client;
+using Sem_Veterina.CRUD;
+using Sem_Veterina.Entity;
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
-namespace Sem_Veterina
+var builder = WebApplication.CreateBuilder(args);
+
+// Pøidání connection stringu
+string connectionString = builder.Configuration.GetConnectionString("OracleDbContext");
+Console.WriteLine(connectionString);
+
+// Konfigurace DbContext pro Oracle s použitím connection stringu
+builder.Services.AddDbContext<OracleDbContext>(options =>
+    options.UseOracle(connectionString));
+builder.Services.AddTransient<DbTestService>();
+builder.Services.AddTransient<KlinikaService>();
+builder.Services.AddTransient<ZvireService>();
+builder.Services.AddTransient<MajitelService>();
+builder.Services.AddTransient<ZdravotniAkceService>(); // Pøidejte tuto øádku pro ZdravotniAkceService
+builder.Services.AddTransient<PristrojeService>();
+builder.Services.AddTransient<LecbaService>();
+builder.Services.AddTransient<LekyService>();
+builder.Services.AddTransient<PersonalService>();
+
+// Pøidání MVC a dalších služeb
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Testování pøipojení k databázi
+using (var connection = new OracleConnection(connectionString))
 {
-    public class Program
+    try
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        connection.Open();
+        Console.WriteLine("Pøipojení k databázi bylo úspìšné!");
     }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Pøipojení k databázi se nezdaøilo.");
+        Console.WriteLine($"Chyba: {ex.Message}");
+    }
+}
+
+// Možnost inicializace DbContextu pøi spuštìní aplikace
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<OracleDbContext>();
+        // Pøípadná inicializace databáze nebo migrace
+        // context.Database.Migrate();
+
+        // Získání instance KlinikaService ZVÍØATA DI kontejneru
+        var klinikaService = services.GetService<KlinikaService>();
+        if (klinikaService != null)
+        {
+            // Volání metody GetAllKlinikyAsync a výpis do konzole
+            await DisplayAllKliniky(klinikaService);
+        }
+        else
+        {
+            Console.WriteLine("KlinikaService nelze naèíst z DI kontejneru.");
+        }
+        List<ZVIRATA> ZvireList = await services.GetService<ZvireService>().GetAllZvirataAsync();
+        foreach(var zvirata in ZvireList) { Console.WriteLine(zvirata.DRUH); }
+        
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error during database initialization: {ex.Message}");
+    }
+}
+
+// Konfigurace HTTP request pipeline
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
+// Asynchronní metoda pro výpis všech klinik
+static async Task DisplayAllKliniky(KlinikaService klinikaService)
+{
+    try
+    {
+        List<KLINIKY> kliniky = await klinikaService.GetAllKlinikyAsync();
+
+        Console.WriteLine("Seznam klinik:");
+        foreach (var klinika in kliniky)
+        {
+            Console.WriteLine($"ID: {klinika.ID_KLINIKA}, Název: {klinika.NÁZEV}, Adresa: {klinika.ADRESA}, Telefon: {klinika.TELEFONNÍ_ÈÍSLO}, Email: {klinika.EMAIL}");
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Chyba pøi naèítání klinik: {ex.Message}");
+    }
+   
 }
--- Sem_Veterina/Startup.cs
+++ Sem_Veterina/Startup.cs
@@ -1,146 +1,58 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.EntityFrameworkCore;
-using Sem_Veterina;
+﻿using Microsoft.EntityFrameworkCore;
 using Sem_Veterina.Controllers;
-using Oracle.ManagedDataAccess.Client;
-using Sem_Veterina.CRUD;
-using Sem_Veterina.Entity;
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
-public class Startup
+namespace Sem_Veterina
 {
-    public IConfiguration Configuration { get; }
-
-    public Startup(IConfiguration configuration)
+    public class Startup
     {
-        Configuration = configuration;
-    }
+        public IConfiguration Configuration { get; }
 
-    public void ConfigureServices(IServiceCollection services)
-    {
-        // Přidání connection stringu
-        string connectionString = Configuration.GetConnectionString("OracleDbContext");
-        Console.WriteLine(connectionString);
-
-        // Konfigurace DbContext pro Oracle
-        services.AddDbContext<OracleDbContext>(options =>
-            options.UseOracle(connectionString));
-
-        // Registrace služeb do DI kontejneru
-        services.AddTransient<DbTestService>();
-        services.AddTransient<KlinikaService>();
-        services.AddTransient<ZvireService>();
-        services.AddTransient<MajitelService>();
-        services.AddTransient<ZdravotniAkceService>();
-        services.AddTransient<PristrojeService>();
-        services.AddTransient<LecbaService>();
-        services.AddTransient<LekyService>();
-        services.AddTransient<PersonalService>();
-
-        // Přidání MVC a dalších služeb
-        services.AddControllersWithViews();
-    }
-
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-        // Testování připojení k databázi
-        TestDatabaseConnection(Configuration.GetConnectionString("OracleDbContext")).Wait();
-
-        // Inicializace služeb a databáze
-        InitializeDatabase(app).Wait();
-
-        if (!env.IsDevelopment())
+        public Startup(IConfiguration configuration)
         {
-            app.UseExceptionHandler("/Home/Error");
-            app.UseHsts();
+            Configuration = configuration;
         }
 
-        app.UseHttpsRedirection();
-        app.UseStaticFiles();
-        app.UseRouting();
-        app.UseAuthorization();
-
-        app.UseEndpoints(endpoints =>
+        public void ConfigureServices(IServiceCollection services)
         {
-            endpoints.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
-        });
-    }
+            services.AddDbContext<OracleDbContext>(options =>
+                options.UseOracle(Configuration.GetConnectionString("OracleDbContext")));
+            // Registrovat DbContext
 
-    private static async Task TestDatabaseConnection(string connectionString)
-    {
-        using var connection = new OracleConnection(connectionString);
-        try
-        {
-            connection.Open();
-            Console.WriteLine("Připojení k databázi bylo úspěšné!");
+
+            // Registrovat DbTestService jako singleton, scoped nebo transient
+            services.AddTransient<DbTestService>();
+
+            // Přidat MVC služby nebo kontrolery
+
+
+            services.AddControllersWithViews();
         }
-        catch (Exception ex)
-        {
-            Console.WriteLine("Připojení k databázi se nezdařilo.");
-            Console.WriteLine($"Chyba: {ex.Message}");
-        }
-    }
 
-    private static async Task InitializeDatabase(IApplicationBuilder app)
-    {
-        using var scope = app.ApplicationServices.CreateScope();
-        var services = scope.ServiceProvider;
-        try
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            var context = services.GetRequiredService<OracleDbContext>();
-            // Možné spuštění migrací
-            // context.Database.Migrate();
-
-            // Získání instance KlinikaService
-            var klinikaService = services.GetService<KlinikaService>();
-            if (klinikaService != null)
+            if (env.IsDevelopment())
             {
-                await DisplayAllKliniky(klinikaService);
+                app.UseDeveloperExceptionPage();
             }
             else
             {
-                Console.WriteLine("KlinikaService nelze načíst z DI kontejneru.");
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
             }
 
-            var zvireService = services.GetService<ZvireService>();
-            if (zvireService != null)
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
             {
-                List<ZVIRATA> ZvireList = await zvireService.GetAllZvirataAsync();
-                foreach (var zvirata in ZvireList)
-                {
-                    Console.WriteLine(zvirata.DRUH);
-                }
-            }
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Error during database initialization: {ex.Message}");
-        }
-    }
-
-    private static async Task DisplayAllKliniky(KlinikaService klinikaService)
-    {
-        try
-        {
-            List<KLINIKY> kliniky = await klinikaService.GetAllKlinikyAsync();
-
-            Console.WriteLine("Seznam klinik:");
-            foreach (var klinika in kliniky)
-            {
-                Console.WriteLine($"ID: {klinika.ID_KLINIKA}, Název: {klinika.NÁZEV}, Adresa: {klinika.ADRESA}, Telefon: {klinika.TELEFONNÍ_ČÍSLO}, Email: {klinika.EMAIL}");
-            }
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Chyba při načítání klinik: {ex.Message}");
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
