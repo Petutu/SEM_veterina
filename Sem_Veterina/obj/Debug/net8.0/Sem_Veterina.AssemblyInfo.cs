5e68a43398427cf81af700a0ee9761d5fbe21236
AnastasiiaAnosova <119511898+Anastasiia-Anosova@users.noreply.github.com>
2024-11-07 00:39:42 +0100

    Added styles for filter in Admin-> Kliniky

--- Sem_Veterina/obj/Debug/net8.0/Sem_Veterina.AssemblyInfo.cs
+++ Sem_Veterina/obj/Debug/net8.0/Sem_Veterina.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sem_Veterina")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d97db215701f6c3a2be2e8bad636138b8b5e2d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c30c3eb3aabec7ae57f56119d34421c2699665dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sem_Veterina")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sem_Veterina")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
